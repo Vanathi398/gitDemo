ae53380ee344e17c3f63e20caec78d991fec7725
vdevendran2 <vdevendran2@192.168.9.17>
2025-09-04 12:27:01 +0530

    welcome commit

--- gitdemo/src/gitdemo/Welcome.java
+++ gitdemo/src/gitdemo/Welcome.java
@@ -5,7 +5,9 @@ public class Welcome {
 	public static void main(String[]args) {
 		
 		System.out.println("Welcome to git");
+		System.out.println("pushing the code to github");
 		System.out.println("Pulling the code to github");
+		System.out.println("Pulling the code to remote: attempt2");
 		
 	}
 
