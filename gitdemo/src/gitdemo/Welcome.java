3bbc2010063527cef2131a7a4650ecf042b532bc
vdevendran2 <vdevendran2@192.168.9.17>
2025-09-04 11:53:50 +0530

    gitDemo commit.

---
+++ gitdemo/src/gitdemo/Welcome.java
@@ -0,0 +1,11 @@
+package gitdemo;
+
+public class Welcome {
+	
+	public static void main(String[]args) {
+		
+		System.out.println("Welcome to git");
+		System.out.println("Pushing the code to github");
+	}
+
+}
